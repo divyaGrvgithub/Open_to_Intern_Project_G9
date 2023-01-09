9ee7679a199e9f98d414d7ab35afc3145f59d3f9
Sumit kumar <sumokhan76342@gmail.com>
2023-01-09 16:47:48 +0530

    final

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ mongoose.connect("mongodb+srv://divyamala_:Dt25042000knp@divyamala.0cofsch.mongo
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
 
-app.use(
-    function (req, res, next) {
-        console.log("inside GLOBAL MW");
-        next();
-    }
-);
+// app.use(
+//     function (req, res, next) {
+//         console.log("inside GLOBAL MW");
+//         next();
+//     }
+// );
 
 app.use('/', route);
 
